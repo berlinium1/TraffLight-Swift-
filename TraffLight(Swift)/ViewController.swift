2e4ed9daf4dd36c093ad64093a03456852e1a3b6
berlinsky <berliniumyar@gmail.com>
2021-03-14 01:57:02 +0200

    Initial Commit

---
+++ TraffLight(Swift)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraffLight(Swift)
+//
+//  Created by Берлинский Ярослав Владленович on 14.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
