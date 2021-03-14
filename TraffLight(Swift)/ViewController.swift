41e5359fd37f57d9ccd4ff2afb661ce2fb336269
berlinsky <berliniumyar@gmail.com>
2021-03-14 02:54:49 +0200

    Design to main added

--- TraffLight(Swift)/ViewController.swift
+++ TraffLight(Swift)/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeColor() {
+    }
+    
 }
 
